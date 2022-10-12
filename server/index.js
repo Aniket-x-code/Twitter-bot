9e5821d96cacb05773c5f96f9581f42dcd9cf6a0
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2022-10-13 00:11:52 +0530

    adding data

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const needle = require('needle');
-const config = require('dotenv').config()
+const config = require('dotenv').config();
 const TOKEN = process.env.TWITTER_BEARER_TOKKEN
 
 const rulesURL='https://api.twitter.com/2/tweets/search/stream/rules'
 const streamURL='https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id'
 
-const rules=[{value: 'giveaway'}, { value: 'xbox'}]
+const rules=[{value: 'giveaway'}, { value: 'xbox '}];
 
 
 // get stream rules
