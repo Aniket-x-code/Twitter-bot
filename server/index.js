129570c2aa18796456fb9b5e3ef8e4eb8113c7c6
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2022-10-15 20:52:48 +0530

    fetching live time tweets

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const config = require('dotenv').config();
 const TOKEN = process.env.TWITTER_BEARER_TOKKEN
 
 const rulesURL='https://api.twitter.com/2/tweets/search/stream/rules'
-const streamURL='https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id'
+const streamURL='https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id'
 
-const rules=[{value: 'giveaway'}, { value: 'xbox '}];
+const rules=[{value: 'worldcup'}];
 
 
 // get stream rules
@@ -16,7 +16,7 @@ async function getRules()
             Authorization: `Bearer ${TOKEN}`
         }
     }) 
-    console.log(response.body);
+    // console.log(response.body);
     return response.body;
 }
 
@@ -36,9 +36,18 @@ async function setRules()
 
 
 // delete stream rules
-async function setRules()
-{   const data = {
-    add: rules
+async function deleteRules(rules)
+{   
+    if(!Array.isArray(rules.data)){
+        return null
+    }
+    
+    const ids = rules.data.map((rule)=> rule.id)
+
+    const data = {
+    delete: {
+        ids: ids
+    }
 }
     const response= await needle('post', rulesURL,data, {
         headers:{
@@ -49,15 +58,39 @@ async function setRules()
     return response.body;
 }
 
+function streamTweets()
+{
+    const stream = needle.get(streamURL, { 
+        headers: {
+        Authorization: `Bearer ${TOKEN}`
+                 }
+        })
+        stream.on('data', (data)=>{
+            try {
+                const json = JSON.parse(data)
+                console.log(json)
+            } catch (error) {
+                
+            }
+        })
+}
+
 
 (async()=>{
      let currentRules
 
      try {
-        await setRules()
+        // get all stream rules
         currentRules = await getRules()
+        // delete all stream rules
+        
+        await deleteRules(currentRules)
+        
+        await setRules()
+      
      } catch ( error) {
         console.log(error);
         process.exit(1)
      }
+     streamTweets()
 })()
